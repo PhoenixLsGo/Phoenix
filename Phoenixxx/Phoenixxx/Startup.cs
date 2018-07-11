9f7e7018300f1f222807d47022d8caa1bc3e20f0
吕石林 <15216123273>
2018-07-12 01:54:24 +0800

    ok

---
+++ Phoenixxx/Phoenixxx/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Phoenixxx.Startup))]
+namespace Phoenixxx
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
